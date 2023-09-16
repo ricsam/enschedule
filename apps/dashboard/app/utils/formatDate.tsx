428ed87657b1a78ab411331788f5d94e18ccddf8
Richie <ricsam@users.noreply.github.com>
2023-09-16 20:45:54 +0200

    prevent hydration errors by saying less than a minute instead of reporting seconds in tables

--- apps/dashboard/app/utils/formatDate.tsx
+++ apps/dashboard/app/utils/formatDate.tsx
@@ -1,13 +1,21 @@
-import { formatDuration, intervalToDuration, format } from 'date-fns';
+import { formatDuration, intervalToDuration, format } from "date-fns";
 
 export const formatDate = (
   _date: Date | string,
   verbs?: { future: string; past: string } | false
 ): { label: string; value: string } => {
-  const date = typeof _date === 'string' ? new Date(_date) : _date;
+  const date = typeof _date === "string" ? new Date(_date) : _date;
   const start = new Date();
   const duration = intervalToDuration({ start, end: date });
-  let unit: (keyof Duration)[] = ['years', 'months', 'weeks', 'days', 'hours', 'minutes', 'seconds'];
+  let unit: (keyof Duration)[] = [
+    "years",
+    "months",
+    "weeks",
+    "days",
+    "hours",
+    "minutes",
+    "seconds",
+  ];
   for (let i = 0; i < unit.length; i += 1) {
     const fmt = unit[i];
     if (duration[fmt]) {
@@ -15,18 +23,21 @@ export const formatDate = (
       break;
     }
   }
-  const value = format(date, 'yyyy-MM-dd:HH:mm:ss');
-  const time = formatDuration(duration, {
+  const value = format(date, "yyyy-MM-dd:HH:mm:ss");
+  let time = formatDuration(duration, {
     format: unit,
   });
-  let label = time ? `${time} ago` : 'just now';
+  if (unit[0] === "seconds") {
+    time = "less than a minute";
+  }
+  let label = time ? `${time} ago` : "just now";
   if (verbs === false) {
     if (start <= date) {
       label = `in ${time}`;
     }
   } else {
-    let past = 'ran';
-    let future = 'run';
+    let past = "ran";
+    let future = "run";
     if (verbs) {
       past = verbs.past;
       future = verbs.future;

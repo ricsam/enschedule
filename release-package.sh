e5a5fe4db9688f4ca1efb57bf77db5e45120173d
Richie <ricsam@users.noreply.github.com>
2023-12-20 08:06:54 +0100

    deploy hub test

--- release-package.sh
+++ release-package.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+set -eo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
 for pkg in worker-api types worker pg-driver hub; do
   jq '.main = "./dist/index.js" | .types = "./dist/index.d.ts"' ./packages/$pkg/package.json | sponge ./packages/$pkg/package.json
 done
